426f4b7d20f6208e6219fbe316ba56882bd767c4
piksellat <piksellat@gmail.com>
2021-01-18 14:43:12 +0100

    qetash

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,44 @@
 import React from "react";
 import PageWrapper from "../components/PageWrapper";
-import Hero from "../sections/landing1/Hero";
+import Hero from "../sections/landing10/Hero";
 import Services from "../sections/landing1/Services";
-import Testimonial1 from "../sections/landing1/Testimonial1";
 import Content1 from "../sections/landing1/Content1";
+import Brands from "../sections/landing12/Brands";
 import Fact from "../sections/landing1/Fact";
 import Feature from "../sections/landing1/Feature";
 import CTA1 from "../sections/landing1/CTA1";
 import CaseStudies from "../sections/landing1/CaseStudies";
-import Testimonial2 from "../sections/landing1/Testimonial2";
+import Video from "../sections/landing8/Video";
+import Testimonial from "../sections/landing6/Testimonial";
 import CTA2 from "../sections/landing1/CTA2";
 
 const IndexPage = () => {
   return (
     <>
       <PageWrapper
+      headerConfig={{
+        theme: "dark",
+        align: "right",
+        button: "cta", // cta, account, null
+        buttonText: "Get a free consultation",
+      }}
         footerConfig={{
-          style: "style2", //style1, style2
+          style: "style1", //style1, style2
         }}
       >
         <Hero />
         <Services />
-        <Testimonial1 />
+        <Brands />
         <Content1 />
         <Fact />
         <Feature />
         <CTA1 />
         <CaseStudies />
-        <Testimonial2 />
+        <br></br>
+        <br></br>
+        <br></br>
+        <Video />
+        <Testimonial />
         <CTA2 />
       </PageWrapper>
     </>
