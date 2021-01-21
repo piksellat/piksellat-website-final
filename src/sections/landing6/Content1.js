85386c172669617194154f1cacc1257112d29fc7
armejndi <itsarmend@gmail.com>
2021-01-21 02:31:02 +0100

    armend was here

--- src/sections/landing6/Content1.js
+++ src/sections/landing6/Content1.js
@@ -10,6 +10,18 @@ const Content = () => {
       {/* <!-- Content Area --> */}
       <div className="content-section bg-default-2 pt-lg-15 pb-13 pb-lg-24">
         <Container>
+        <Row className="justify-content-center">
+          <Col xl="6" lg="7" md="8">
+            <div className="section-title text-center mb-11 mb-lg-21">
+              <h3 className="sub-badge gr-text-12 text-uppercase text-red mb-7">
+                Services
+              </h3>
+              <h2 className="title gr-text-4 mb-0">
+                We offer a great variery of "fuck yes" services
+              </h2>
+            </div>
+          </Col>
+        </Row>
           <Row className="align-items-center justify-content-center">
             <Col sm="10" lg="7">
               <div
@@ -28,8 +40,8 @@ const Content = () => {
             <Col sm="10" lg="5">
               <div className="content-text pt-11 pt-lg-0 pl-xl-7">
                 <h2 className="gr-text-4 mb-7">
-                  Track your progress <br className="d-none d-lg-block" />
-                  with our advanced site.
+                  Kick-ass <br className="d-none d-lg-block" />
+                  brand identities.
                 </h2>
                 <p className="gr-text-8 mb-0">
                   We share common trends and strategies for improving your
@@ -37,10 +49,10 @@ const Content = () => {
                 </p>
                 <Link
                   to="/#"
-                  className="btn-link with-icon gr-text-blue gr-text-7 font-weight-bold mt-9"
+                  className="btn-link with-icon gr-text-blue gr-text-7 mt-9"
                 >
-                  Start a free trial{" "}
-                  <i className="icon icon-tail-right font-weight-bold"></i>
+                  See our works{" "}
+                  <i className="icon icon-tail-right"></i>
                 </Link>
               </div>
             </Col>
