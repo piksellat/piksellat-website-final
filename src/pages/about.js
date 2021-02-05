f8f8b1e81d4a64b889f1898067fb6fb8ef5b1d1d
pixendrit <pixendrit@outlook.com>
2021-02-05 17:23:29 +0100

    rreth nesh 0.1

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,11 +34,10 @@ const AboutPage = () => {
             <Row className="justify-content-center mt-md-6 pt-24 pt-lg-29">
               <Col lg="9" xl="8">
                 <div className="px-md-12 text-center mb-11 mb-lg-14">
-                  <h2 className="title gr-text-2 mb-9 mb-lg-12">Our story</h2>
+                  <h2 className="title gr-text-2 mb-9 mb-lg-12">We are Piksellat!</h2>
                   <p className="gr-text-8 mb-0">
-                    With lots of unique blocks, you can easily build a page
-                    without coding. Build your next consultancy website within
-                    few minutes.
+                  Our studio is a homely and fun space. Our expert team is made up of creatives with technical know-how,
+                  designers who think outside the box, and developers who push innovation.
                   </p>
                 </div>
               </Col>
@@ -56,93 +55,22 @@ const AboutPage = () => {
               <Col lg="6" className="mb-7 mb-lg-0">
                 <div className="pr-xl-13">
                   <h2 className="gr-text-3 mb-0">
-                    We are here to help the customers to get their success.
+                    Beliefs, Mission & Process
                   </h2>
                 </div>
               </Col>
               <Col lg="6">
                 <div className="pr-xl-13">
                   <p className="gr-text-8 mb-7 mb-lg-10">
-                    We share common trends and strategies for improving your
-                    rental income and making sure you stay in high demand of
-                    service.{" "}
+                  We believe that great design solutions are first a product of intense research and being able to analyze complex subjects and see the ‘big picture’.
+
+This allows us to zoom in, see & create the smaller design blocks.
+{" "}
                   </p>
                   <p className="gr-text-8 mb-0">
-                    With lots of unique blocks, you can easily build a page
-                    without coding. Build your next landing page. With lots of
-                    unique blocks, you can easily build a page without coding
-                    any other page.
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <div className="content-section pt-14 pt-lg-27 pb-13 pb-lg-27 bg-default-2">
-          <Container>
-            <Row className="align-items-center justify-content-center">
-              <Col sm="8" lg="5" xl="6" className="mb-9 mb-lg-0">
-                <div className="content-grid-image-group d-flex  mx-sm-n3">
-                  <div className="single-image d-flex flex-column px-3 px-sm-6">
-                    <img
-                      src={imgC1}
-                      alt=""
-                      data-aos="zoom-in"
-                      data-aos-duration="500"
-                      className="w-100 pb-6 pb-sm-9 rounded-10"
-                    />
+                  For us, it's highly important to approach projects with wide-eyes and open-mind, to discover potential in unexpected places and craft memorable experiences.
+During our conversations with our clients we like to stick to what matters while also being relaxed enough to make doing business with us fun. 
 
-                    <img
-                      src={imgC2}
-                      alt=""
-                      data-aos="zoom-in"
-                      data-aos-duration="500"
-                      data-aos-delay="800"
-                      className="w-100 pb-6 pb-sm-9 rounded-10"
-                    />
-                  </div>
-                  <div className="single-image d-flex flex-column pt-11 px-3 px-sm-6">
-                    <img
-                      src={imgC3}
-                      alt=""
-                      data-aos="zoom-in"
-                      data-aos-duration="500"
-                      data-aos-delay="400"
-                      className="w-100 pb-6 pb-sm-9 rounded-10"
-                    />
-
-                    <img
-                      src={imgC4}
-                      alt=""
-                      data-aos="zoom-in"
-                      data-aos-duration="500"
-                      data-aos-delay="1200"
-                      className="w-100 rounded-10"
-                    />
-                  </div>
-                </div>
-              </Col>
-              <Col
-                sm="8"
-                lg="6"
-                xl="5"
-                className="offset-lg-1 offset-lg-1 offset-xl-0"
-              >
-                <div className="content-text">
-                  <h2 className="gr-text-3 mb-7 mb-lg-11">
-                    High skilled coders <br className="d-none d-lg-block" />{" "}
-                    from worldwide.
-                  </h2>
-                  <p className="gr-text-8 mb-7 mb-lg-10">
-                    We share common trends and strategies for improving your
-                    rental income and making sure you stay in high demand of
-                    service.{" "}
-                  </p>
-                  <p className="gr-text-8 mb-0">
-                    With lots of unique blocks, you can easily build a page
-                    without coding. Build your next landing page. With lots of
-                    unique blocks, you can easily build a page without coding
-                    any other page.
                   </p>
                 </div>
               </Col>
