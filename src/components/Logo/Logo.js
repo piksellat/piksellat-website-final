e877a07d25dcb896682bf6f8200c7bde08e8d764
pixendrit <pixendrit@outlook.com>
2021-02-25 09:37:11 +0100

    added logo as svg. Works like a charm

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import imgL1Logo from "../../assets/image/logo-main-black.png";
-import imgL1LogoWhite from "../../assets/image/logo-main-white.png";
+import imgL1Logo from "../../assets/image/svg/logowhite.svg";
+import imgL1LogoWhite from "../../assets/image/svg/logodark.svg";
 
 const Logo = ({ white, height, className = "", ...rest }) => {
   return (
     <Link to="/" className={`d-block ${className}`} {...rest}>
       {white ? (
-        <img src={imgL1LogoWhite} alt="" />
-      ) : (
         <img src={imgL1Logo} alt="" />
+      ) : (
+        <img src={imgL1LogoWhite} alt="" />
       )}
     </Link>
   );
